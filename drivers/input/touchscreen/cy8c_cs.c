7ee64798a0c667f9cef97f65d1c8e37e44b58622
Davide Rombolà <davide.rombola@gmail.com>
2014-04-22 19:49:40 +0200

    cy8c_cs: proper handling of powersuspend
    Conflicts:
    	drivers/input/touchscreen/cy8c_cs.c

--- drivers/input/touchscreen/cy8c_cs.c
+++ drivers/input/touchscreen/cy8c_cs.c
@@ -41,9 +41,7 @@ struct cy8c_cs_data {
 	struct workqueue_struct *cy8c_wq;
 	struct work_struct work;
 #ifdef CONFIG_POWERSUSPEND
-	struct power_suspend early_suspend;
-#else
-	struct early_suspend early_suspend;
+	struct power_suspend power_suspend;
 #endif
 	int use_irq;
 	struct hrtimer timer;
@@ -565,7 +563,6 @@ enableirq:
 		enable_irq(cs->client->irq);
 }
 
-#if 1
 static enum hrtimer_restart cy8c_cs_timer_func(struct hrtimer *timer)
 {
 	struct cy8c_cs_data *cs;
@@ -574,8 +571,7 @@ static enum hrtimer_restart cy8c_cs_timer_func(struct hrtimer *timer)
 	queue_work(cs->cy8c_wq, &cs->work);
 	return HRTIMER_NORESTART;
 }
-#endif
-#if 1
+
 static irqreturn_t cy8c_cs_irq_handler(int irq, void *dev_id)
 {
 	struct cy8c_cs_data *cs = dev_id;
@@ -584,7 +580,6 @@ static irqreturn_t cy8c_cs_irq_handler(int irq, void *dev_id)
 	queue_work(cs->cy8c_wq, &cs->work);
 	return IRQ_HANDLED;
 }
-#endif
 
 static int cy8c_cs_probe(struct i2c_client *client,
 					const struct i2c_device_id *id)
@@ -687,10 +682,9 @@ static int cy8c_cs_probe(struct i2c_client *client,
 	cs->early_suspend.suspend = cy8c_cs_early_suspend;
 	cs->early_suspend.resume  = cy8c_cs_late_resume;
 #if defined(CONFIG_POWERSUSPEND)
-	register_power_suspend(&cs->early_suspend);
-#else
-	register_early_suspend(&cs->early_suspend);
-#endif
+	cs->power_suspend.suspend = cy8c_cs_power_suspend;
+	cs->power_suspend.resume  = cy8c_cs_power_resume;
+	register_power_suspend(&cs->power_suspend);
 #endif
 	cy8c_touchkey_sysfs_init();
 
@@ -735,9 +729,7 @@ static int cy8c_cs_remove(struct i2c_client *client)
 
 	cy8c_touchkey_sysfs_deinit();
 #ifdef CONFIG_POWERSUSPEND
-	unregister_power_suspend(&cs->early_suspend);
-#else
-	unregister_early_suspend(&cs->early_suspend);
+	unregister_power_suspend(&cs->power_suspend);
 #endif
 	free_irq(client->irq, cs);
 	input_unregister_device(cs->input_dev);
@@ -802,12 +794,16 @@ static void cy8c_cs_late_resume(struct early_suspend *h)
 #ifdef CONFIG_POWERSUSPEND
 static void cy8c_cs_early_suspend(struct power_suspend *h)
 {
-	scr_suspended = true;
+	struct cy8c_cs_data *ts;
+	ts = container_of(h, struct cy8c_cs_data, power_suspend);
+	cy8c_cs_suspend(ts->client, PMSG_SUSPEND);
 }
 
 static void cy8c_cs_late_resume(struct power_suspend *h)
 {
-	scr_suspended = false;
+	struct cy8c_cs_data *ts;
+	ts = container_of(h, struct cy8c_cs_data, power_suspend);
+	cy8c_cs_resume(ts->client);
 }
 #endif
 
@@ -819,7 +815,7 @@ static struct i2c_driver cy8c_cs_driver = {
 	.probe		= cy8c_cs_probe,
 	.remove		= cy8c_cs_remove,
 	.id_table	= cy8c_cs_id,
-#ifndef CONFIG_HAS_EARLYSUSPEND
+#ifndef CONFIG_POWERSUSPEND
 	.suspend	= cy8c_cs_suspend,
 	.resume		= cy8c_cs_resume,
 #endif
